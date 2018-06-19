2a901f69be0c36efcee5cfe2e2a94170a98e73ef
Carmen Shing <carmenshing@gmail.com>
2018-06-19 21:46:38 +0000

    updated char-counter function, counter now works with negative number

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-    // --- our code goes here ---
-    console.log('document is ready for DOM events')
-  });
+
+    $(".textarea").keyup(function() {
+        const counter = $(this).closest(".new-tweet").find(".counter"); 
+        const charLength = $(this).val().length;
+        const totalChar = 140 - charLength;
+
+        counter.text(totalChar);
+
+        if (totalChar < 0) {
+            counter.addClass("negative");
+        } else {
+            counter.removeClass("negative");
+        }
+    })
+})
+
+
+
+// $(textarea).addEventListener('keyup', function(e) {
+//     console.log("hi")
+//   });
